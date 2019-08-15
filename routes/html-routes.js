f9c74ff97f20ee4cd47f19c7ccc1a8c93a35cc16
WCW789 <wcw789@gmail.com>
2019-08-15 17:52:27 -0500

    Revised

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,23 +1,23 @@
 let path = require("path");
 
 module.exports = function (app) {
-  // app.get("/", function (req, res) {
-  //   console.log('We hit the landing page');
-  //   res.sendFile(path.join(__dirname, "../public/appTemplate/code/dark/index.html"));
-  // });
-
   app.get("/", function (req, res) {
-    console.log('We hit link1');
+    console.log('We hit the homepage');
+    res.sendFile(path.join(__dirname, "../public/appTemplate/code/dark/index.html"), {});
+  });
+
+  app.get("/about", function (req, res) {
+    console.log('We hit the about page');
     res.sendFile(path.join(__dirname, "../public/appTemplate/code/dark/index-circle.html"), {});
   });
 
-  app.get("/link2", function (req, res) {
-    console.log('We hit link2');
+  app.get("/contact", function (req, res) {
+    console.log('We hit the contact link');
     res.sendFile(path.join(__dirname, "../public/appTemplate/code/dark/page-contact.html"), {});
   });
 
-  app.get("/link3", function (req, res) {
-    console.log('We hit link3');
+  app.get("/thankyou", function (req, res) {
+    console.log('We hit the thank you link');
     res.sendFile(path.join(__dirname, "../public/appTemplate/code/dark/page-thanks.html"), {});
   });
 };
